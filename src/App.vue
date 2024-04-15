7229ad2651cc7fb0d45138f00b80e9c9f4acef42
simopucci <simo.pucci7@gmail.com>
2024-04-15 18:16:55 +0200

    feat: stampo in pagina i progetti tramite chiamata api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <script>
+  import axios from 'axios';
+
     export default {
       data() {
         return {
-          title:"Boolfolio"
-        }
+          title:"Boolfolio",
+          projects: [],
+        };
+      },
+
+      created() {
+        axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+          this.projects = res.data.data
+        });
       }
+
     }
 </script>
 
 <template>
-  <h1> {{ title }}</h1>
+  <div class="container mt-5">
+    <h1 class="mb-4"> {{ title }}</h1>
+
+    <div v-for="project in projects" class="mb-3 border-bottom">
+      <ul>
+        <li><strong>ID: </strong>{{ project.id }}</li>
+        <li><strong>Title: </strong>{{ project.title }}</li>
+        <li><strong>Github Link: </strong>{{ project.github_reference }}</li>
+        <li><strong>Description: </strong>{{ project.description }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
