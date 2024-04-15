a934994f42a8c55c9c5efc31a9d90fb84765353c
simopucci <simo.pucci7@gmail.com>
2024-04-15 19:58:49 +0200

    feat: add pagination and fetch method

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -8,27 +8,40 @@
         data() {
             return {               
                 store,
+                pagination: [],
             };
+        },
+
+        methods: {
+            fetchProjects(endpoint = api.baseUrl + 'projects') {
+                axios.get(endpoint).then((res) => {
+                    store.projects = res.data.data;
+                    this.pagination = res.data.links;
+                });
+            },
         },
 
         components: { ProjectCard },
 
         created() {
-            axios.get(api.baseUrl + 'projects').then((res) => {
-                store.projects = res.data.data;
-            });
+            this.fetchProjects();
         },
     };
 </script>
 
 <template>
-    
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li @click="fetchProjects(link.url)" :class="{ active: link.active, disabled: !link.url, }" v-for="link in pagination" class="page-item">
+                <a class="page-link" href="#" v-html="link.label"></a>
+            </li>
+        </ul>
+    </nav>
+
     <div class="row row-cols-4 g-3">
         <project-card v-for="project in store.projects" :project="project"></project-card>
-        
     </div>
-
-
+        
 </template>
 
 <style lang="scss">
